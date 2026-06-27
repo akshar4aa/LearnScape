16771a23be469f9c4c03bf26df24034317c1da4f
Akshara <aksharakalivemula2526@gmail.com>
2026-06-27 16:18:45 +0530

    "Created game engine and window"

---
+++ src/engine/game.py
@@ -0,0 +1,42 @@
+import pygame
+
+from src.engine.settings import *
+from src.states.state import State
+
+
+class Game:
+
+    def __init__(self):
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption(TITLE)
+
+        self.clock = pygame.time.Clock()
+
+        self.running = True
+
+        self.state = State(self)
+
+    def run(self):
+
+        while self.running:
+
+            dt = self.clock.tick(FPS) / 1000
+
+            events = pygame.event.get()
+
+            for event in events:
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            self.state.handle_events(events)
+            self.state.update(dt)
+
+            self.screen.fill(BACKGROUND_COLOR)
+
+            self.state.draw(self.screen)
+
+            pygame.display.flip()
+
+        pygame.quit()
---
+++ src/engine/settings.py
@@ -0,0 +1,9 @@
+# Window Settings
+WIDTH = 1280
+HEIGHT = 720
+
+TITLE = "LearnScape"
+
+FPS = 60
+
+BACKGROUND_COLOR = (20, 24, 35)
